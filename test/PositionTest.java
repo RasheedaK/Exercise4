21fe2d6be1651ef9ca2cba0478a7e2b04f819e71
krashee <krashee@thoughtworks.com>
2017-02-13 15:01:03 +0530

    Add tests for position's getters

--- test/PositionTest.java
+++ test/PositionTest.java
@@ -14,4 +14,19 @@ public class PositionTest {
         Position position1=new Position(1,2, 'N');
         assertNotEquals(position,position1);
     }
+    @Test
+    public void testxCorodinateFor1IfPositionIs12N() {
+        Position position=new Position(1,2, 'N');
+        assertTrue(1==position.getXcoordinate());
+    }
+    @Test
+    public void testyCorodinateFor2IfPositionIs12N() {
+        Position position=new Position(1,2, 'N');
+        assertTrue(2==position.getYcoordinate());
+    }
+    @Test
+    public void testDirectionForNIfPositionIs12N() {
+        Position position=new Position(1,2, 'N');
+        assertTrue('N'==position.getDirection());
+    }
 }
