e39bf045c898885aaa5298f51e3b708adc659774
krashee <krashee@thoughtworks.com>
2017-02-13 12:23:36 +0530

    Rename roverPositon class to position and add equals method to position class

---
+++ src/Position.java
@@ -0,0 +1,26 @@
+class RoverPosition {
+    private final int xCoordinate;
+    private final int yCoordinate;
+
+    RoverPosition(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RoverPosition position = (RoverPosition) o;
+
+        return xCoordinate == position.xCoordinate && yCoordinate == position.yCoordinate;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = xCoordinate;
+        result = 31 * result + yCoordinate;
+        return result;
+    }
+}
--- src/RoverPosition.java
+++
@@ -1,9 +0,0 @@
-class RoverPosition {
-    private final int xCoordinate;
-    private final int yCoordinate;
-
-    RoverPosition(int xCoordinate, int yCoordinate) {
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-    }
-}
