8ca692564469ef8742abf9d0232e3d8e7bd5c5eb
krashee <krashee@thoughtworks.com>
2017-02-15 17:11:10 +0530

    modify tests for rover

--- test/RoverTest.java
+++ test/RoverTest.java
@@ -4,39 +4,39 @@ import static org.junit.Assert.assertEquals;
 
 public class RoverTest {
    @Test
-    public void testPosition12NIfRoverIsAt12EForInstrutionL() {
-        Position roverPosition = new Position(1, 2);
-        Direction direction= new Direction('E');
+    public void testPosition12IfRoverIsAt11NForInstrutionM() {
+        Position roverPosition = new Position(1, 1);
+        Direction direction= new Direction('N');
         Rover rover = new Rover(roverPosition,direction);
-        rover.explore("L");
+        rover.explore("M");
         String expectedString="1 2";
         assertEquals(expectedString,rover.getPosition());
     }
     @Test
-    public void testPosition13NIfRoverIsAt12EForInstrutionM() {
+    public void testPosition02IfRoverIsAt12WForInstrutionM() {
         Position roverPosition = new Position(1, 2);
-        Direction direction= new Direction('N');
+        Direction direction= new Direction('W');
         Rover rover = new Rover(roverPosition,direction);
         rover.explore("M");
-        String expectedString="1 3";
+        String expectedString="0 2";
         assertEquals(expectedString,rover.getPosition());
     }
     @Test
-    public void testPosition32SIfRoverIsAt32EForInstrutionR() {
+    public void testPosition31IfRoverIsAt32SForInstrutionM() {
         Position roverPosition = new Position(3, 2);
-        Direction direction= new Direction('E');
+        Direction direction= new Direction('S');
         Rover rover = new Rover(roverPosition,direction);
-        rover.explore("R");
-        String expectedString="3 2";
+        rover.explore("M");
+        String expectedString="3 1";
         assertEquals(expectedString,rover.getPosition());
     }
     @Test
-    public void testPosition32SIfRoverIsAt32EForInstrutionRLLR() {
+    public void testPosition42IfRoverIsAt32EForInstrutionM() {
         Position roverPosition = new Position(3, 2);
         Direction direction= new Direction('E');
         Rover rover = new Rover(roverPosition,direction);
-        rover.explore("R");
-        String expectedString="3 2";
+        rover.explore("M");
+        String expectedString="4 2";
         assertEquals(expectedString,rover.getPosition());
     }
 }
