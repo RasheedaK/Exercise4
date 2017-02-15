2b8618a3effc08d3ae8699e95032ab39f0b32f68
krashee <krashee@thoughtworks.com>
2017-02-15 18:44:13 +0530

    Modify main class

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
         String[] strings=input.split(" ");
         int xCoordinate=Integer.parseInt(strings[0]);
         int yCoordinate=Integer.parseInt(strings[1]);
-        char dir=strings[2].charAt(0);
+        char roverDirection=strings[2].charAt(0);
         Position position=new Position(xCoordinate,yCoordinate);
+        Direction direction=new Direction(roverDirection);
+        Rover rover=new Rover(position,direction);
+        rover.explore("LMLMLMLMM");
+        System.out.println(rover.getPosition());
+        System.out.println(rover.getDirection());
     }
 }
