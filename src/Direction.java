9f408a3a9dd381c5124dd17b80d70c57e78e3821
krashee <krashee@thoughtworks.com>
2017-02-15 22:20:10 +0530

    Replace magic characters for direction with constants

--- src/Direction.java
+++ src/Direction.java
@@ -1,4 +1,8 @@
 class Direction {
+    static final char NORTH='N';
+    static final char SOUTH='S';
+    static final char EAST='E';
+    static final char WEST='W';
     private final char direction;
     Direction(char direction){
         this.direction=direction;
--- src/Rover.java
+++ src/Rover.java
@@ -25,33 +25,33 @@ class Rover {
     }
 
     private Position moveForward() {
-        if (direction.getDirection() == 'E')
+        if (direction.getDirection() == Direction.EAST)
             return position.getRightPosition();
-        if (direction.getDirection() == 'W')
+        if (direction.getDirection() == Direction.WEST)
             return position.getLeftPosition();
-        if (direction.getDirection() == 'N')
+        if (direction.getDirection() == Direction.NORTH)
             return position.getTopPosition();
         else
             return position.getDownPosition();
     }
 
     private Direction turnLeft() {
-        if (direction.getDirection() == 'E')
+        if (direction.getDirection() == Direction.EAST)
             return direction.getNorth();
-        if (direction.getDirection() == 'W')
+        if (direction.getDirection() == Direction.WEST)
             return direction.getSouth();
-        if (direction.getDirection() == 'N')
+        if (direction.getDirection() == Direction.NORTH)
             return direction.getWest();
         else
             return direction.getEast();
     }
 
     private Direction turnRight() {
-        if (direction.getDirection() == 'E')
+        if (direction.getDirection() == Direction.EAST)
             return direction.getSouth();
-        if (direction.getDirection() == 'W')
+        if (direction.getDirection() == Direction.WEST)
             return direction.getNorth();
-        if (direction.getDirection() == 'N')
+        if (direction.getDirection() == Direction.NORTH)
             return direction.getEast();
         else
             return direction.getWest();
