6d048af6628a0a28fec80c805170b28b7f4eb2eb
krashee <krashee@thoughtworks.com>
2017-02-16 17:21:41 +0530

    Create InputConsole class for taking input and add a try catch block for handling invalid input

---
+++ src/InputConsole.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class InputConsole {
+    Scanner sc=new Scanner(System.in);
+    String takeInput(){
+        return sc.nextLine();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        InputConsole inputConsole=new InputConsole();
+        String input=inputConsole.takeInput();
         DirectionTeller directionTeller = new DirectionTeller();
         directionTeller.iniatializeMap();
         Grid grid = new Grid(5, 5);
-        InputParser inputParser=new InputParser();
-        Rover rover=inputParser.parseInput(input);
-        rover.explore("LMLMLMLMM", grid, directionTeller);
-        System.out.println(rover.getPosition());
-        System.out.println(rover.getDirection());
+        try {
+            InputParser inputParser = new InputParser();
+            Rover rover = inputParser.parseInput(input);
+            rover.explore("LMLMLMLMM", grid, directionTeller);
+            System.out.println(rover.getPosition());
+            System.out.println(rover.getDirection());
+        }catch (Exception e){
+            System.out.println("Invalid Input");
+        }
     }
 }
